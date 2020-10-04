a5a520d9eeb791d8b4b4d08591ba5e32393aafe3
juanjo giraldez <juanjose.giraldez@kairosds.com>
2020-10-04 12:42:03 +0200

    start proyect BackEnd Go, add files, main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main;
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+
+	route := mux.NewRouter()
+
+	server := http.Server{
+
+		Addr: ":3306",
+		Handler: route,
+	}
+
+	log.Println("Running on port 3306")
+
+	log.Println(server.ListenAndServe())
+}
